d256a25d38aff0318c95f39132748c9949a6680f
Luca Ordronneau <ordronneau@eisti.eu>
2018-12-04 08:56:40 +0100

    ajout des types joueurs

---
+++ projet.ml
@@ -0,0 +1,11 @@
+#use "code.ml";;
+#use "IA.ml";;
+module Mastermind = struct
+	
+type joueur = {nom : string; points : int; humain : bool};;
+type joueurs = joueur list;;
+
+
+
+end;;
+open Mastermind;;
