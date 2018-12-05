522fda9f64168ce612cb9f7e93827f8d0ca6cadc
Johan Moncoutié <moncoutiej@eisti.eu>
2018-12-05 11:48:13 +0100

    ajout string of code et code ofstring + les fonctions associées

--- code.ml
+++ code.ml
@@ -2,7 +2,7 @@
 module Code :
 sig
 (** Le type d'un pion *)
-type pion = (* A COMPLETER *)
+type pion = int
 (** Le type d'un code *)
 type t = pion list
 (** Nombre de pions par code *)
@@ -29,9 +29,9 @@ val string_of_code : t -> string
 *)
 val code_of_string : string -> t option
 (** La liste de tous les codes permis *)
-val tous : t list
+val tous : t list;;
 (** La liste de toutes les reponses possibles *)
-val toutes_reponses : (int * int) list ;;
+val toutes_reponses : (int * int) list 
 (** Calcule la reponse d'un code par rapport au code cache
 * @param code le code propose
 * @param vrai_code le code cache
@@ -39,5 +39,50 @@ val toutes_reponses : (int * int) list ;;
 [None] si la reponse ne peut etre calculee
 *)
 val reponse : t -> t -> (int * int) option
+end =
+struct 
+
+let rec string_of_code_rec t = 
+	"|"^
+	match t with
+	| [] -> ""
+	| 0 :: liste -> "orange"^string_of_code_rec liste
+	| 1 :: liste -> "rouge"^string_of_code_rec liste
+	| 2 :: liste -> "vert"^string_of_code_rec liste
+	| 3 :: liste -> "bleu"^string_of_code_rec liste
+	| 4 :: liste -> "jaune"^string_of_code_rec liste
+	| 5 :: liste -> "violet"^string_of_code_rec liste
+	| 6 :: liste -> "blanc"^string_of_code_rec liste
+	| 7 :: liste -> "cyan"^string_of_code_rec liste;;
+
+let string_of_code t = string_of_code_rec t ^ "|";;
+
+let num_of_couleur_opt s = 
+	match s with
+	| "orange" -> Some(0) 
+	| "rouge" -> Some(1) 
+	| "vert" -> Some(2) 
+	| "bleu" -> Some(3) 
+	| "jaune" -> Some(4) 
+	| "violet" -> Some(5) 
+	| "blanc" -> Some(6) 
+	| "cyan" -> Some(7) 
+	| _ -> None;;
+
+let rec code_of_string s =
+	if s = "|" then
+	 Some([])
+	else
+	 let i_deuxieme_separateur = String.index_from s 1 '|' in
+	  let s_prem_couleur = String.sub s 1 (i_deuxieme_separateur -1) in
+	   let suite_s = String.sub s i_deuxieme_separateur (String.rindex s '|' -i_deuxieme_separateur +1) in
+	    let pion = num_of_couleur_opt s_prem_couleur in
+	     let apl_rec = code_of_string suite_s in
+			match (pion,apl_rec) with
+			| (Some(a),Some(b)) -> Some(a :: b)
+			| _ -> None;;
+
 end ;;
 
+
+"|rouge|bleu|gfgff|gg|"
