82f050e4c5326d66fa4bb4e55e70d2f1078db5b3
Luca Ordronneau <ordronneau@eisti.eu>
2018-12-05 17:23:23 +0100

    Changement une liste de joueurs a couple de joueurs

--- projet.ml
+++ projet.ml
@@ -1,4 +1,3 @@
-#use "code.ml";;
 #use "IA.ml";;
 module Mastermind = struct
 	
@@ -6,14 +5,12 @@ type joueur = {nom : string; points : int; humain : bool};;
 type joueurs = joueur list;;
 
 let creationjoueurs mode = if mode then 
-{nom = read_line (print_string "Entrer votre nom : "); points = 0; humain = true} :: [{nom = "Ordinateur"; points = 0; humain =false}] else 
-{nom = read_line (print_string "Entrer votre nom joueur 2 : "); points = 0; humain = true} :: [{nom = read_line (print_string "Entrer votre nom joueur 1 : "); points = 0; humain =true}];;
+({nom = read_line (print_string "Entrer votre nom : "); points = 0; humain = true}, {nom = "Ordinateur"; points = 0; humain =false}) else 
+({nom = read_line (print_string "Entrer votre nom joueur 2 : "); points = 0; humain = true}, {nom = read_line (print_string "Entrer votre nom joueur 1 : "); points = 0; humain =true});;
 
-let commence listejoueurs = if Random.bool() then match listejoueurs with
-                                                  | e :: t -> [e]
-                            else match listejoueurs with
-                                  | e :: t -> t;;
-
+let commence couplejoueurs = let b = Random.bool() in match couplejoueurs with
+															| (_,_) when b -> couplejoueurs
+															| (x,y) -> (y,x);;
 
 
 let rec code_egaux code1 code2 acc =
@@ -22,7 +19,7 @@ let rec code_egaux code1 code2 acc =
 	| (a :: ls1, b :: ls2) -> if a = b then code_egaux ls1 ls2 (acc+1) 
 							  else code_egaux ls1 ls2 acc;;
 
-(*let rec ajout_points acc = if code_egaux code1*)
+
 
 
 
